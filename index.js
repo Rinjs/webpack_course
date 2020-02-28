431c78a0f4ede899e109e2c2432a7b348f29f16e
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-02-28 16:54:47 +0300

    added simple webpack config

--- index.js
+++
@@ -1,3 +0,0 @@
-const post = new Post('Webpack post title')
-
-console.log(post.toString())
---
+++ src/Post.js
@@ -0,0 +1,13 @@
+export default class Post {
+  constructor(title) {
+    this.title = title;
+    this.date = new Date().toLocaleString();
+  }
+
+  toString() {
+    return JSON.stringify({
+      title: this.title,
+      date: this.date
+    });
+  }
+}
