ec2a6b4e20d842e7bc630832e2ac99ed45bf4fff
Vladislav Korenik <vladislavkorenik@gmail.com>
2020-02-28 17:45:00 +0300

    Added: loaders for csv, xml and object alias for path through @

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Post from "./Post";
 
-import Logo from "./assets/logo.png";
-import json from "./assets/json";
+import Logo from "@assets/logo";
+import json from "@assets/json";
+import xml from "@assets/data.xml";
+import csv from "@assets/data.csv";
 import "./styles/styles.css";
 
 const post = new Post(json.title, Logo);
 
+console.log("XML", xml);
+console.log("CSV", csv);
 console.log(post.toString());
